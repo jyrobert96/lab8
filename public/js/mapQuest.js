515adfc5726fdce34bf396c4dac6720b86c80784
Josh Robertson <jyrobert@ucsd.edu>
2019-02-28 13:58:26 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '4UxsFDeHHIJxy9Zlm0fwJFZH1aTSUXAE';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878875, -117.237015],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878875, -117.237015]).addTo(map);
 }
